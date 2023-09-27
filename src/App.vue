ac00ef20e3c805ca48c2434e366845abe0599c52
yunqing <realyunq@gmail.com>
2023-09-27 10:54:15 +0800

    temp commit

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,32 @@
 body {
   @apply bg-gray-100;
 }
+
+::-webkit-scrollbar {
+  width: 0px;
+  background-color: transparent;
+}
+
+::-webkit-scrollbar-corner {
+  display: block;
+}
+
+::-webkit-scrollbar:hover {
+  width: 10px;
+  height: 10px;
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  background-color: #4B5563;
+  border: 2px solid transparent;
+  background-clip: padding-box;
+}
+
+::-webkit-scrollbar-thumb,
+::-webkit-scrollbar-track {
+  border-left-color: transparent;
+  border-right-color: transparent;
+  /* background-color: #4c5562; */
+}
 </style>
--- src/pages/pic/list.vue
+++ src/pages/pic/list.vue
@@ -1,17 +1,65 @@
 <template>
-        <el-container class="bg-white rounded">
-            <el-header>Header</el-header>
-            <el-container>
-                <el-aside width="200px">Aside</el-aside>
-                <el-main>Main</el-main>
-            </el-container>
+    <el-container class="bg-white rounded" :style="{ height: (h + 'px') }">
+        <el-header class="i-header">Header</el-header>
+        <el-container>
+            <el-aside width="200px" class="i-aside">
+                <div class="i-a-top">
+                    <div v-for="i in 100" :key="i">{{ i }}</div>
+                </div>
+                <div class="i-a-bottom">
+                    分页区域
+                </div>
+            </el-aside>
+            <el-main class="i-main">
+                <div class="i-m-top">
+                    <div v-for="i in 100" :key="i">{{ i }}</div>
+                </div>
+                <div class="i-m-bottom">
+                    分页区域
+                </div>
+            </el-main>
         </el-container>
+    </el-container>
 </template>
 
 <script setup>
-
+const windowHeight = window.innerHeight || document.body.clientHeight
+// 减去 页面标题header、标签header、上下border size
+const h = windowHeight - 64 - 44 - 40
 </script>
 
 <style scoped>
+.i-header {
+    border-bottom: 1px solid #eeeeee;
+    @apply flex items-center;
+}
 
+.i-aside {
+    border-right: 1px solid #eeeeee;
+    position: relative;
+}
+
+.i-main {
+    position: relative;
+}
+
+.i-a-top,
+.i-m-top {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 50px;
+    overflow-y: auto;
+}
+
+.i-a-bottom,
+.i-m-bottom {
+    position: absolute;
+    height: 50px;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    @apply flex items-center justify-center;
+}
 </style>
